3974f8a780e58e6b7aa7989e3125632433a9c90e
Mai <jea989@gmail.com>
2022-11-04 15:34:29 -0400

    preliminary selection

--- vc2.js
+++ vc2.js
@@ -11,7 +11,7 @@ cPD.style.left = "0";
 cPD.style.right = "0";
 cPD.appendChild(document.createTextNode("Control Panel"));
 
-const strokes = ['lineTo', 'quadraticCurveTo'];
+const strokes = ['lineTo', 'selection', 'quadraticCurveTo'];
 const strokeSelectList = document.createElement("select");
 strokeSelectList.id = "strokeSelectList";
 cPD.appendChild(strokeSelectList);
@@ -56,6 +56,7 @@ document.body.appendChild(canv);
 const cursor = {
   x: (innerWidth / 2),
   y: (innerHeight / 2),
+  mousedown: false,
 };
 
 
@@ -63,24 +64,37 @@ const cursor = {
 addEventListener("mousemove", (e) => {
   cursor.x = Math.floor(e.clientX - canv.getBoundingClientRect().left);
   cursor.y = Math.floor(e.clientY - canv.getBoundingClientRect().top);
-
 });
 
+/*
+addEventListener("mousedown", (e) => {
+  cursor.mousedown = true;
+});
 
+addEventListener("mouseup", (e) => {
+  cursor.mousedown = false;
+});
+*/
+
+//addEventListener("click", (e) => {
+//  window.requestAnimationFrame(draw);
+//  clicked = true;
+//  console.log(e);
+//  console.log("e.timeStamp:", e.timeStamp, "local timeStamp:", timeStamp);
+//});
 
 const ctx = canv.getContext('2d');
+ctx.lineWidth = 5;
 const funcQ = [];
 const clickArr = [];
 
 let clickCounter = 0;
 let mtox = 0;
 let mtoy = 0;
+let selected = null;
 
 canv.onclick = () => {
   clickCounter++;
-  clickArr.push({x: cursor.x, y: cursor.y});
-  cAL = clickArr.length;
-  cAI = cAL - 1;
   if (strokeSelectList.value === 'lineTo') {
     if (clickCounter === 1) {
       mtox = cursor.x;
@@ -94,22 +108,53 @@ canv.onclick = () => {
         ctx.stroke();
       })
     } else if (clickCounter === 2) {
-      const mx = mtox;
-      const my = mtoy
-      const lx = cursor.x;
-      const ly = cursor.y; 
+      let mx = mtox;
+      let my = mtoy
+      let lx = cursor.x;
+      let ly = cursor.y; 
       funcQ.pop();
+      //funcQ.push(() => {
+      const strokeIndex = funcQ.length;
+      const strokeCreationTime = timeStamp;
       funcQ.push(() => {
         ctx.beginPath();
         ctx.moveTo(mx, my);
         ctx.lineTo(lx, ly);
+        if (strokeSelectList.value === 'selection') {
+            if (ctx.isPointInStroke(cursor.x, cursor.y)) {
+              selected === strokeCreationTime ? ctx.strokeStyle = 'Green' : ctx.strokeStyle = 'RoyalBlue';
+              if (clickCounter === 1) {
+                selected = strokeCreationTime;
+                clickCounter = 0
+              }
+            } else if (selected === strokeCreationTime) {
+              ctx.strokeStyle = 'DarkGreen';
+              const sr = new Path2D();
+              sr.rect(mx - 3, my - 3, 6, 6)
+              if (ctx.isPointInPath(sr, cursor.x, cursor.y)) {
+                ctx.strokeStyle = 'White';
+                if (clickCounter === 1) {
+                  mx = cursor.x;
+                  my = cursor.y;
+                }
+              } else {
+                ctx.strokeStyle = 'DarkGreen';
+              }
+              ctx.stroke(sr)
+              ctx.strokeStyle = 'DarkGreen';              
+            } else {
+              ctx.strokeStyle = 'Blue';
+            }
+        }
         ctx.stroke();
       })
       clickCounter = 0;
     }
+  } else if (strokeSelectList.value === 'selection') {
+//    selected = null;
+//    clickCounter = 0;
   }
 }
-
 
 let timeStamp = 0;
 let secondsPassed = 0;
@@ -120,13 +165,13 @@ const displayStatus = () => {
   oldTimeStamp = timeStamp;
   fps = Math.round(1 / secondsPassed);
 
-  const statusString = `X:${String(cursor.x).padStart(4, '0')} Y: ${String(cursor.y).padStart(4, '0')} FPS: ${fps}`
+  const statusString = `X:${String(cursor.x).padStart(4, '0')} Y: ${String(cursor.y).padStart(4, '0')} FPS:${fps}`
 
   ctx.save();
   ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.font = '16px Monospace';
   ctx.fillStyle = 'yellow';
-  ctx.fillText(statusString, canv.width * .8, canv.height * .99);
+  ctx.fillText(statusString, canv.width * .81, canv.height * .99);
   ctx.restore();
 }
 
