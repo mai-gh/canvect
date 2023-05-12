7c8da7577e6002bedb6f5ca128943b6fe6fbd217
Mai <jea989@gmail.com>
2023-05-12 10:32:42 -0400

    more buttons

--- canv/controlPanel.js
+++ canv/controlPanel.js
@@ -1,24 +1,53 @@
 import { deselectAll } from './select.js'
 
-const strokes = ["Select", "Line", "Circle"];
+const strokes = ["Scroll", "Select", "Line", "Circle", 'Rectangle', 'Multi-Line', 'Arc', 'Elipse', 'Quadratic Bézier', 'Cubic Bézier', 'Text'];
+
+const options = ['Show Grid', 'Snap to Grid', 'Snap to Edges', 'Import SVG', 'Export SVG', 'CLI']
 
 const genCPD = (co) => {
   const cPD = document.createElement("div");
+  const toolPanel = document.createElement("div");
+  const optionsPanel = document.createElement("div");
+  //const debugPanel = document.createElement("div");
+
+ 
   cPD.id = "controlPanelDiv";
+  toolPanel.id = "toolPanelDiv";
+  optionsPanel.id = "optionsPanelDiv";
+  //debugPanel.id = "debugPanelDiv";
+
   strokes.forEach( val => {
     const b = document.createElement("button");
     b.id = `${val}-Button`
     b.classList.add('toolButton');
     b.innerHTML = val;
     b.onclick = (e) => {
-      document.querySelectorAll('.toolButton').forEach(e => e.classList.             remove('toolButtonSelected'));
+      document.querySelectorAll('.toolButton').forEach(e => e.classList.remove('toolButtonSelected'));
       e.target.classList.add('toolButtonSelected')
       deselectAll(co);
       co.clickCounter = 0;
       co.selectedTool = val;
     }
-    cPD.appendChild(b);
+    //cPD.appendChild(b);
+    toolPanel.appendChild(b);
   });
+
+  options.forEach( val => {
+    const b = document.createElement("button");
+    b.id = `${val}-Button`
+    b.classList.add('toolButton');
+    b.innerHTML = val;
+    optionsPanel.appendChild(b);
+  });
+
+
+  //optionsPanel.appendChild(document.createTextNode("OPTIONS"));
+  //debugPanel.appendChild(document.createTextNode("DEBUG"));
+
+  cPD.appendChild(toolPanel);
+  cPD.appendChild(optionsPanel);
+  //cPD.appendChild(debugPanel);
+
   return cPD
 }
 
